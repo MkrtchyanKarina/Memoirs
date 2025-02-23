97a747dd3988ca41dc0b9e99fa8097c7cee3650a
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2025-02-23 17:13:57 +0300

    created model for post essence and started creating home page

--- memoirs/post/models.py
+++ memoirs/post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """ Класс, описывающий структуру таблицы в БД. Экземпляр этого класса - конкретная запись в созданной таблице """
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)  # blank=True - поле необязательно для заполнения
+    time_create = models.DateTimeField(auto_now_add=True)  # auto_now_add=True - поле задается только
+    # при создании записи и больше не изменяется
+    time_update = models.DateTimeField(auto_now=True)  # auto_now=True - поле меняется при каждом изменении записи
+    is_published = models.BooleanField(default=True)  # default=True - по умолчанию статья опубликована
+
+    def __str__(self):
+        return self.title
---
+++ memoirs/post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, register_converter
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index),
+
+]
