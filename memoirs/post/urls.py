15bbf5e3afcbc188e262dbe2f3d9073189ab2517
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2025-02-26 17:09:50 +0300

    Created header in base template and added css styles for it. Also added necessary pictures.

--- memoirs/post/urls.py
+++ memoirs/post/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home_page'),
+    path('about/', views.about, name="about"),
+    # path('post/<int:post_id>/', views.show_post, name="post"),
+    path('add-post/', views.add_post, name="add_post"),
+    path('contact/', views.contact, name="contact"),
+    path('login/', views.login, name="login"),
 
 ]
--- memoirs/post/views.py
+++ memoirs/post/views.py
@@ -3,9 +3,39 @@ from django.shortcuts import get_list_or_404, render
 from .models import Post
 
 
+menu = [
+    {'title': "Главная страница", 'url_name': 'home_page'},
+    {'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить пост", 'url_name': 'add_post'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'},
+]
+
 
 
 def index(request):
-    return render(request, 'post/index.html')
+    data = {
+        'title': 'home page',
+        'menu': menu,
+        }
+    return render(request, 'post/index.html', context=data)
+
+
+def about(request):
+    return HttpResponse("<h1>Информация о сайте</h1>")
+    # data = {'title': 'О сайте', 'menu': menu}
+    # return render(request, 'posts/about.html', context=data)
+
+
+def add_post(request):
+    return HttpResponse("<h1>Добавление нового поста в блог</h1>")
+
+
+def login(request):
+    return HttpResponse("<h1>Войти или зарегистрироваться</h1>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Обратная связь</h1>")
 
 
