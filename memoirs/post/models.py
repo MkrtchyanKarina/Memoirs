1c70c29e16b0d24990e9bcd13035096307f9f00b
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2025-03-08 00:19:46 +0300

    Created a new model "Category" in the "Post" app and a new field (foreign key) "cat_id" in the "Post" model. issue #8

--- memoirs/post/models.py
+++ memoirs/post/models.py
@@ -7,18 +7,25 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(is_published=Post.Status.PUBLISHED)
 
 
+
 class Post(models.Model):
+    """ Класс, описывающий структуру таблицы в БД. Экземпляр этого класса - конкретная запись в созданной таблице """
     class Status(models.IntegerChoices):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
-    """ Класс, описывающий структуру таблицы в БД. Экземпляр этого класса - конкретная запись в созданной таблице """
+
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)  # blank=True - поле необязательно для заполнения
     time_create = models.DateTimeField(auto_now_add=True)  # auto_now_add=True - поле задается только
     # при создании записи и больше не изменяется
     time_update = models.DateTimeField(auto_now=True)  # auto_now=True - поле меняется при каждом изменении записи
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)  # default=True - по умолчанию статья опубликована
+    is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)  # default=True - по умолчанию статья опубликована
+
+    cat = models.ForeignKey(
+        "Category",
+        on_delete=models.PROTECT,
+    )  # cat - это полноценная запись из таблицы/ объект класса Category! А cat_id - это только поле - идентификатор записи
 
     objects = models.Manager()
     published = PublishedManager()
@@ -34,3 +41,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
