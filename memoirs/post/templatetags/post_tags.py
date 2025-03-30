fc5a40a3255ca37847298907a0fba75fefe1096e
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2025-03-30 14:15:43 +0300

    Added display of only used categories and tags

--- memoirs/post/templatetags/post_tags.py
+++ memoirs/post/templatetags/post_tags.py
@@ -1,16 +1,18 @@
 from django import template
 import post.views as views
+from django.db.models import Count
 from post.models import Category, TagPost
 register = template.Library()
 
 
+
 @register.inclusion_tag('post/list_categories.html')
 def show_categories(cat_selected=0):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(total=Count('posts')).filter(total__gt=0)
     return {'categories': categories, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('post/list_tags.html')
 def show_all_tags():
-    tags = TagPost.objects.all()
+    tags = TagPost.objects.annotate(total=Count('tags')).filter(total__gt=0)
     return {'tags': tags}
